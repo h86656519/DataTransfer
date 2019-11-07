97ece6d745e2aa1773349ebae4caa628eb653e76
h86656519 <h86656519@gmail.com>
2019-11-08 00:32:45 +0800

    新增註解201911081232

--- common/src/main/java/com/example/common/Person2.java
+++ common/src/main/java/com/example/common/Person2.java
@@ -2,6 +2,11 @@ package com.example.common;
 
 import java.io.Serializable;
 
+
+/*嘗試用一個共同的package 來做讓 app 和 getmessage 共用Person2，
+因為app 的 Person2 和 getmessage 的 Person2 會被視為不一樣的東西
+ps:  app 和 getmessage 都要先 implementation project(':common')
+* */
 public class Person2 implements Serializable {
     private String name;
     private int ID;
